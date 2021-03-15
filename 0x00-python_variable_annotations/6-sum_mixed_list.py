6ebfd6f30c8dc98d3934fae9b9c0b209dce282d6
Yurei <yosribouabid@gmail.com>
2021-03-15 20:46:31 +0100

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""6-sum_of_list"""
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """
+    Return the sum of a mixed list
+    """
+    return sum(mxd_lst)
