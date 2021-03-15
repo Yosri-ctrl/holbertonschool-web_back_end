86c071a4cb1edcc7a4add3fe8a831c41ab38f527
Yurei <yosribouabid@gmail.com>
2021-03-15 22:33:21 +0100

    task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""First Element"""
+from typing import Mapping, Any, Union, TypeVar
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[TypeVar('T'),
+                                    None]=None) -> Union[Any, TypeVar('T')]:
+    """Returns The value"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
