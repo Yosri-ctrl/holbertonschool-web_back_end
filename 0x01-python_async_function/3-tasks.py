b6ebe77ec4ec6c70ca0c8bb46fa0aa8e5fdf065f
Yurei <yosribouabid@gmail.com>
2021-03-17 11:29:06 +0100

    documentation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,5 +5,9 @@ from time import time
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int = 10) -> Task:
+def task_wait_random(max_delay: int) -> Task:
+    """
+    Take the max_delay and pass it tp wait_random
+    and Return it
+    """
     return (create_task(wait_random(max_delay)))
