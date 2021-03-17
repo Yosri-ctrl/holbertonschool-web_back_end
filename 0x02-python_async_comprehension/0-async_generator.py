dd7a4c2e595c4e6a4939a7cced69f0c9645c5c91
Yurei <yosribouabid@gmail.com>
2021-03-17 15:17:05 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+"""
+Wait for 1 second
+And generat a float between 0 and 10
+"""
 import asyncio
 import random
 from typing import List
 
 
 async def async_generator() -> List[float]:
-
+	"""
+	Generate a float between 0 and 10
+	And return it
+	"""
 	for _ in range(10):
 		await asyncio.sleep(1)
 		yield random.uniform(0, 10)
