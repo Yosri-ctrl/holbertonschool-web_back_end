e107d1d1d260416636c056373eadcbb7e822ceda
Yurei <yosribouabid@gmail.com>
2021-03-20 20:37:05 +0100

    task 0

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Creating a new class BasicCache 
+Inhereted from BaseCaching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Creat a cashing system
+    Inherited form BaseCaching
+    """
+    def put(self, key, item):
+        """
+        Append to cashe_data
+        in key position with item as value
+        """
+        if key or item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """
+        Return content of cache_data
+        And None if key is none
+        """
+        if key:
+            return self.cache_data.get(key)
+        else:
+            return None
