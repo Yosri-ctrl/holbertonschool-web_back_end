0262b47f8d9e0356ef3f2b61e01f1d44695720df
Yurei <yosribouabid@gmail.com>
2021-03-28 15:44:37 +0100

    pycode

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,30 +4,17 @@ returns the log message obfuscated
 """
 from typing import List
 import re
-import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields, redaction, message, separator):
     """
     Take str message and return it obfuscated
     fields contain the field to obfuscated by redaction
+    print(message[pas[0]:pas[1] + a[1] -1])
     """
     for field in fields:
         pas = re.search(field, message).span()
         a = re.search(separator, message[pas[1]:]).span()
-        message = re.sub(message[pas[1] + 1: pas[1] + a[1] - 1], redaction, message)
+        change = message[pas[1] + 1: pas[1] + a[1] - 1]
+        message = re.sub(change, redaction, message)
     return message
