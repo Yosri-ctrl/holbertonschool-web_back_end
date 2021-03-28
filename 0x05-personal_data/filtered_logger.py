e1fb74cbc7d0572f3e072bddd1c5549efd2d494a
Yurei <yosribouabid@gmail.com>
2021-03-28 15:25:59 +0100

    task 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-Main file
+returns the log message obfuscated
 """
 from typing import List
 import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Take str message and return it obfuscated
+    fields contain the field to obfuscated by redaction
+    """
     for field in fields:
         pas = re.search(field, message).span()
         a = re.search(separator, message[pas[1]:]).span()
