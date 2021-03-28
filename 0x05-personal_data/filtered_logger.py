cf176f24c4381aa2e58eab67146d5825581ac6f1
Yurei <yosribouabid@gmail.com>
2021-03-28 15:34:55 +0100

    seperator fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,7 +4,22 @@ returns the log message obfuscated
 """
 from typing import List
 import re
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
@@ -14,5 +29,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     for field in fields:
         pas = re.search(field, message).span()
         a = re.search(separator, message[pas[1]:]).span()
-        message = re.sub(message[pas[1] + 1: pas[1] + a[1]], redaction, message)
+        message = re.sub(message[pas[1] + 1: pas[1] + a[1] - 1], redaction, message)
     return message
