32adbbb9bc6f65c9523855208a39f84d86a0ddea
Yurei <yosribouabid@gmail.com>
2021-03-29 13:03:53 +0100

    pycode

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,11 +6,11 @@ from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Take str message and return it obfuscated
     fields contain the field to obfuscated by redaction
-    print(message[pas[0]:pas[1] + a[1] -1])
     """
     for field in fields:
         pas = re.search(field, message).span()
