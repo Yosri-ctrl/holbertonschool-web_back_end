3cb8d67d27beefbe2adaf76c465976f616475108
Yurei <yosribouabid@gmail.com>
2021-03-29 20:57:51 +0100

    task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,7 +4,24 @@ returns the log message obfuscated
 """
 from typing import List
 import re
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+         return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -14,7 +31,6 @@ def filter_datum(fields: List[str], redaction: str,
     """
     for field in fields:
         pas = re.search(field, message).span()
-        a = re.search(separator, message[pas[1]:]).span()
-        change = message[pas[1] + 1: pas[1] + a[1] - 1]
-        message = re.sub(change, redaction, message)
+        a = re.search(separator, message[pas[1] + 1:]).span()
+        message = re.sub(message[pas[1] + 1: pas[1] + a[1]], redaction, message)
     return message
