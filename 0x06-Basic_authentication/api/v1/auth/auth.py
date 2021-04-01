3f1a204aabc4932042097a61a227dc9a590ba4df
Yurei <yosribouabid@gmail.com>
2021-04-01 21:03:43 +0100

    pycodestyle

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 """
-_
+This class is the template for all
+authentication system you will implement.
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    """
+    This class is the template for all
+    authentication system you will implement.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Validate if path exist in excluded_paths
+        and return bool
         """
-        """
-        if path is not None and  not path.endswith("/"):
+        if path is not None and not path.endswith("/"):
             path += "/"
-            
-        if path is None or excluded_paths is None or path not in excluded_paths:
+
+        if path is None or excluded_paths is None or
+        path not in excluded_paths:
             return True
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         """
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         """
         return None
-
-     
--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -32,6 +32,7 @@ def unauthorized() -> None:
     """
     return abort(401)
 
+
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> None:
     """ Forcibly raise 401 error code
