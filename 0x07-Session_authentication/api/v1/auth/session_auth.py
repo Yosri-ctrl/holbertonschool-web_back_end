7a4064dabb341102133293fe31e056c2f63a7561
Yurei <yosribouabid@gmail.com>
2021-04-07 20:21:48 +0100

    task 0-4

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""
+"""class SessionAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
  
 
 class SessionAuth(Auth):
-    """
+    """class SessionAuth that inherits from Auth
+    for creating a new authentication mechanism
     """
     user_id_by_session_id = {}
     def create_session(self, user_id: str = None) -> str:
+        """
+        """
         if user_id is None or type(user_id) is not str:
             return None
         id = str(uuid4())
@@ -18,6 +21,8 @@ class SessionAuth(Auth):
     
     
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        """
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
