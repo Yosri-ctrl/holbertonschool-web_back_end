a422c98950636dbb9f5779608d7b3cad70a9ccd8
Yurei <yosribouabid@gmail.com>
2021-04-14 16:55:48 +0100

    user fix

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""User Model
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    """
+    """User class
     """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
