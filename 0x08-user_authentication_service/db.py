0fb37a90dcb1b6624f431eb4c8532925a4392d12
Yurei <yosribouabid@gmail.com>
2021-04-14 17:07:22 +0100

    fix sql error

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -32,6 +32,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Add_user to User database
         """
