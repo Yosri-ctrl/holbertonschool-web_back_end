b0d1deab02d9ce4ecb2946dd7e22269ef8016f95
Yurei <yosribouabid@gmail.com>
2021-04-15 16:28:43 +0100

    task 16

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -68,6 +68,17 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt
