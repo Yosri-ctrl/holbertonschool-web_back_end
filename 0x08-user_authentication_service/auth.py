2ed0b7a1a2848429669f124e33aa20ca6b7dbb35
Yurei <yosribouabid@gmail.com>
2021-04-15 15:13:36 +0100

    task 8

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Hash a password using bcrypt
 """
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -25,6 +25,15 @@ class Auth:
             return user
         else:
             raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Returns
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), user.hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
