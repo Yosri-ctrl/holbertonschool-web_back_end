5ba446f65c2886e8e5e44f67f4be8f0af5090632
Yurei <yosribouabid@gmail.com>
2021-04-22 15:12:39 +0100

    task 0

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""task 0
 """
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    """
+    """return page containig header
     """
     return render_template("0-index.html")
 
