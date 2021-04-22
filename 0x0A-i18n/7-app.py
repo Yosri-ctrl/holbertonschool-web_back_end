5d4b881737e5bca84b459421bad53a9dc7e2a107
Yurei <yosribouabid@gmail.com>
2021-04-22 17:41:27 +0100

    Task 7

---
+++ 0x0A-i18n/7-app.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""task 3
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from pytz import timezone, exceptions
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    """config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+def get_user():
+    """get user function
+    """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """set get_user as a global on g.user
+    """
+    g.user = get_user()
+
+
+@app.route('/')
+def hello_world():
+    """return the render of index page
+    """
+    return render_template("7-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """return the matched language
+    """
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """ get timezone """
+    timezone = request.args.get('timezone')
+    if timezone in pytz.all_timezones:
+        return timezone
+    else:
+        raise pytz.exceptions.UnknownTimeZoneError
+    user = request.args.get('login_as')
+    timezone = users[int(user)]['timezone']
+    if timezone in pytz.all_timezones:
+        return timezone
+    else:
+        raise pytz.exceptions.UnknownTimeZoneError
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+if __name__ == "__main__":
+    app.run()
