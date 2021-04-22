8ac10ad1fa0f0d91c6d05a6a84e0b0f18335e14f
Yurei <yosribouabid@gmail.com>
2021-04-22 15:16:25 +0100

    documentation

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""task 2
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -8,7 +8,7 @@ babel = Babel(app)
 
 
 class Config():
-    """
+    """Config class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -16,15 +16,18 @@ class Config():
 
 
 app.config.from_object(Config)
+
+
 @app.route('/')
 def hello_world():
-    """
+    """return page contain header
     """
     return render_template("2-index.html")
 
+
 @babel.localeselector
 def get_locale():
-    """
+    """return the matched language
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
