6200973c1510d4d98f86f2ba2efa10f1d752d26d
Yurei <yosribouabid@gmail.com>
2021-04-22 17:23:18 +0100

    task 5

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -25,27 +25,11 @@ class Config():
 app.config.from_object(Config)
 
 
-@app.route('/')
-def hello_world():
-    """return the render of index page
-    """
-    return render_template("3-index.html")
-
-
-@babel.localeselector
-def get_locale():
-    """return the matched language
-    """
-    lang = request.args.get('locale')
-    if lang in app.config['LANGUAGES']:
-        return lang
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 def get_user():
     """get user function
     """
     try:
-        return users[int(request.args.get('login_as'))]
+        return users.get(int(request.args.get('login_as')))
     except Exception:
         return None
 
@@ -56,6 +40,21 @@ def before_request():
     """
     g.user = get_user()
 
+@app.route('/')
+def hello_world():
+    """return the render of index page
+    """
+    return render_template("5-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """return the matched language
+    """
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if __name__ == "__main__":
     app.run()
