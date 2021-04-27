612f0a028d17045f3d500419893cc79a893764af
Yurei <yosribouabid@gmail.com>
2021-04-27 23:21:03 +0100

    fix decorator

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -45,6 +45,7 @@ class Cache():
         self._redis.flushdb()
 
     @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the data in redis using rand key
         """
