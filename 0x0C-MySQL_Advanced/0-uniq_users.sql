9384b1fb831d2272cb161bd441fd502bd205a23a
Yurei <yosribouabid@gmail.com>
2021-04-29 17:41:36 +0100

    task 0

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- creat a users table                                          
+-- contain id, email, name                                      
+CREATE TABLE IF NOT EXISTS `users`(
+`id`     int NOT NULL AUTO_INCREMENT,
+`email`  varchar(255) NOT NULL UNIQUE,
+`name`   varchar(255),
+PRIMARY KEY (id)
+);
