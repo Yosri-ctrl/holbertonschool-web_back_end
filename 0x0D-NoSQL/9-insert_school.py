80140e7d0a8ad1a21677e844cd95ca0c4a6c65c6
Yurei <yosribouabid@gmail.com>
2021-05-04 11:21:18 +0100

    taks 9

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs
     """
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert_one(kwargs).inserted_id
