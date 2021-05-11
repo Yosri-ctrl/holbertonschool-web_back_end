ad702e30114558f44ff7ff0db1a843a7eed7d3ca
Yurei <yosribouabid@gmail.com>
2021-05-11 07:39:46 +0100

    no variables

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,5 +1,4 @@
 /*Return a Promise */
 export default function getResponseFromAPI(){
-	const myPromise = new Promise((resolve, reject) => {});
-	return myPromise;
+	return new Promise((resolve, reject) => {});
 }
