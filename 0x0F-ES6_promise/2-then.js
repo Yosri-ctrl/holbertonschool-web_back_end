046c19077869cd716af7d2036fb434f25d3fc48e
Yurei <yosribouabid@gmail.com>
2021-05-11 07:58:47 +0100

    task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,8 +2,6 @@ export default function handleResponseFromAPI(promise) {
 	promise.then(() => ({
 		status: 200,
       	body: 'success',
-	}));
-	promise.catch(() => Error());
-	promise.finally(() => console.warn('Got a response from the API'));
+	})).catch(() => Error()).finally(() => console.warn('Got a response from the API'));
 	return promise;
 }
