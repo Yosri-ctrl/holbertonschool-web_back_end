c44c6c513056a95c09886d92e68497506794831d
Yurei <yosribouabid@gmail.com>
2021-05-12 09:19:09 +0100

    lint fix

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-	const myPromise = new Promise((resolve, reject) => {
-		if (success == true) {
-		resolve({"status": 200, "body": 'Success',});
-		} else {
-		reject(Error('The fake API is not working currently'));
-		}
-	});
-	return myPromise;
+  const myPromise = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return myPromise;
 }
