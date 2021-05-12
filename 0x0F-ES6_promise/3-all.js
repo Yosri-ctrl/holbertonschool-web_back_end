82a1d1deb264e2179d77352ec13a64e485f68686
Yurei <yosribouabid@gmail.com>
2021-05-12 09:58:53 +0100

    lint fix

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-  	const myPromise =  Promise.all([uploadPhoto(), createUser()])
-    myPromise.then((resp) => {
-      const { body } = resp[0];
-      const { firstName, lastName } = resp[1];
-      console.log(`${body} ${firstName} ${lastName}`);
-	});
-    myPromise.catch(() => console.log('Signup system offline'));
-	return myPromise;
+  const myPromise = Promise.all([uploadPhoto(), createUser()]);
+  myPromise.then((resp) => {
+    const { body } = resp[0];
+    const { firstName, lastName } = resp[1];
+    console.log(`${body} ${firstName} ${lastName}`);
+  }).catch(() => console.log('Signup system offline'));
+  return myPromise;
 }
