7c8ff1f4db443660463dea222e78415e40cfcb23
Yurei <yosribouabid@gmail.com>
2021-05-14 22:07:13 +0100

    lint fix

--- 0x10-ES6_classes/8-hbtn_class.js
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -1,11 +1,12 @@
 export default class HolbertonClass {
-	constructor(size, location) {
-		this._size = size;
-		this._location = location;
-	}
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-	[Symbol.toPrimitive](cst) {
-		if (cst === 'number') return this._size;
+  [Symbol.toPrimitive](cst) {
+    if (cst === 'number') return this._size;
     if (cst === 'string') return this._location;
+    return 0;
   }
 }
