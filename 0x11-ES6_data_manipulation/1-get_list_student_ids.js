b608782820431ff2688b960f3b814e38d1579c69
Yurei <yosribouabid@gmail.com>
2021-05-18 08:45:09 +0100

    condition fix

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(array) {
-  if (array.constructor !== Array) { return []; }
+  if (!Array.isArray(array)) { return []; }
   return array.map((x) => x.id);
 }
