f66694841b911340c7903a4bdde916edf0ab4528
Yurei <yosribouabid@gmail.com>
2021-05-18 09:55:43 +0100

    lint fix

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, start) {
-	if (!start.length) return '';
-	let result = '';
-	set.forEach((x) => {
-		if (x.startsWith(start)) result += `${x.slice(start.length)}-`;
-	});
-	return result.slice(0, result.length - 1);
+  if (!start.length) return '';
+  let result = '';
+  set.forEach((x) => {
+    if (x.startsWith(start)) result += `${x.slice(start.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
